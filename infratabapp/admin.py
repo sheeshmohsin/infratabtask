655def3141c9149d78585336c4c27c88829445d3
Sheesh Mohsin <sheeshmohsin@gmail.com>
2016-08-08 18:30:25 +0530

    Registered models to admin

--- infratabapp/admin.py
+++ infratabapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from infratabapp.models import ReminderDetails, EmailNotification, SMSNotification
 
 # Register your models here.
+admin.site.register(ReminderDetails)
+admin.site.register(EmailNotification)
+admin.site.register(SMSNotification)
