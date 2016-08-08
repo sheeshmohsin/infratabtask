6c9ab84d1ac693dae8563ac9df370657b1665ada
Sheesh Mohsin <sheeshmohsin@gmail.com>
2016-08-09 01:16:52 +0530

    Converted indentation to spaces

--- infratabapp/tests.py
+++ infratabapp/tests.py
@@ -8,38 +8,38 @@ client = TestApiClient()
 # Create your tests here.
 class ApiTestCase(TestCase):
 
-	def test_api_home(self):
-		resp = client.get('/api/v1/')
-		self.assertEqual(resp.status_code, 200)
+    def test_api_home(self):
+        resp = client.get('/api/v1/')
+        self.assertEqual(resp.status_code, 200)
 
-	def test_api_emailnotf(self):
-		resp = client.get('/api/v1/emailnotf/')
-		self.assertEqual(resp.status_code, 200)
+    def test_api_emailnotf(self):
+        resp = client.get('/api/v1/emailnotf/')
+        self.assertEqual(resp.status_code, 200)
 
-	def test_api_emailnotf_schema(self):
-		resp = client.get('/api/v1/emailnotf/schema/')
-		self.assertEqual(resp.status_code, 200)
+    def test_api_emailnotf_schema(self):
+        resp = client.get('/api/v1/emailnotf/schema/')
+        self.assertEqual(resp.status_code, 200)
 
-	def test_api_reminder(self):
-		resp = client.get('/api/v1/reminder/')
-		self.assertEqual(resp.status_code, 200)
+    def test_api_reminder(self):
+        resp = client.get('/api/v1/reminder/')
+        self.assertEqual(resp.status_code, 200)
 
-	def test_api_reminder_schema(self):
-		resp = client.get('/api/v1/reminder/schema/')
-		self.assertEqual(resp.status_code, 200)
+    def test_api_reminder_schema(self):
+        resp = client.get('/api/v1/reminder/schema/')
+        self.assertEqual(resp.status_code, 200)
 
-	def test_api_smsnotf(self):
-		resp = client.get('/api/v1/smsnotf/')
-		self.assertEqual(resp.status_code, 200)
+    def test_api_smsnotf(self):
+        resp = client.get('/api/v1/smsnotf/')
+        self.assertEqual(resp.status_code, 200)
 
-	def test_api_smsnotf_schema(self):
-		resp = client.get('/api/v1/smsnotf/schema/')
-		self.assertEqual(resp.status_code, 200)
+    def test_api_smsnotf_schema(self):
+        resp = client.get('/api/v1/smsnotf/schema/')
+        self.assertEqual(resp.status_code, 200)
 
-	def test_api_reminder_save(self):
-		datetimer = datetime.datetime.now() + datetime.timedelta(days=1)
-		resp = client.post('/api/v1/reminder/', data={'datetime': datetimer.utcnow().isoformat(), 'message': 'test message'})
-		self.assertEqual(resp.status_code, 201)
-		resp = client.delete('/api/v1/reminder/', data={'datetime': datetimer.utcnow().isoformat(), 'message': 'test message'})
-		self.assertEqual(resp.status_code, 204)
+    def test_api_reminder_save(self):
+        datetimer = datetime.datetime.now() + datetime.timedelta(days=1)
+        resp = client.post('/api/v1/reminder/', data={'datetime': datetimer.utcnow().isoformat(), 'message': 'test message'})
+        self.assertEqual(resp.status_code, 201)
+        resp = client.delete('/api/v1/reminder/', data={'datetime': datetimer.utcnow().isoformat(), 'message': 'test message'})
+        self.assertEqual(resp.status_code, 204)
 
