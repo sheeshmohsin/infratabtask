c5f7a232bc9c8ed23171f07404b3b78e69661f1c
Sheesh Mohsin <sheeshmohsin@gmail.com>
2016-08-08 18:00:38 +0530

    Added models

--- infratabapp/models.py
+++ infratabapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class ReminderDetails(models.Model):
+    datetime = models.DateTimeField(null=False, blank=False)
+    message = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.message
+
+class EmailNotification(models.Model):
+    reminder = models.ForeignKey(ReminderDetails)
+    email = models.EmailField(blank=False, null=False)
+
+    class Meta:
+        unique_together = (("reminder", "email"),)
+
+    def __str__(self):
+        return self.email
+
+class SMSNotification(models.Model):
+    reminder = models.ForeignKey(ReminderDetails)
+    mobile = models.CharField(max_length=15)
+
+    class Meta:
+        unique_together = (("reminder", "mobile"),)
+
+    def __str__(self):
+        return self.mobile
