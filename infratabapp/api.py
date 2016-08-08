d5601a07ec763eae47122d04a9f4ee6489a3faac
Sheesh Mohsin <sheeshmohsin@gmail.com>
2016-08-08 18:39:42 +0530

    Converted indentation to spaces

--- infratabapp/api.py
+++ infratabapp/api.py
@@ -6,39 +6,39 @@ from infratabapp.models import ReminderDetails, SMSNotification, EmailNotificati
 
 class ReminderDetailsResource(ModelResource):
 
-	class Meta:
-		queryset = ReminderDetails.objects.all()
-		resource_name = "reminder"
-		authorization = Authorization()
-		always_return_data = True
-		cache = SimpleCache(timeout=10)
+    class Meta:
+        queryset = ReminderDetails.objects.all()
+        resource_name = "reminder"
+        authorization = Authorization()
+        always_return_data = True
+        cache = SimpleCache(timeout=10)
 
 class EmailNotificationResource(ModelResource):
-	reminder = fields.ForeignKey(ReminderDetailsResource, 'reminder')
+    reminder = fields.ForeignKey(ReminderDetailsResource, 'reminder')
 
-	class Meta:
-		queryset = EmailNotification.objects.all()
-		resource_name = "emailnotf"
-		excludes = ['id']
-		authorization = Authorization()
-		always_return_data = True
-		cache = SimpleCache(timeout=10)
+    class Meta:
+        queryset = EmailNotification.objects.all()
+        resource_name = "emailnotf"
+        excludes = ['id']
+        authorization = Authorization()
+        always_return_data = True
+        cache = SimpleCache(timeout=10)
 
-	def dehydrate(self, bundle):
-		bundle.data['reminder'] = bundle.obj.reminder.message
-		return bundle
+    def dehydrate(self, bundle):
+        bundle.data['reminder'] = bundle.obj.reminder.message
+        return bundle
 
 class SMSNotificationResource(ModelResource):
-	reminder = fields.ForeignKey(ReminderDetailsResource, 'reminder')
+    reminder = fields.ForeignKey(ReminderDetailsResource, 'reminder')
 
-	class Meta:
-		queryset = SMSNotification.objects.all()
-		resource_name = "smsnotf"
-		excludes = ['id']
-		authorization = Authorization()
-		always_return_data = True
-		cache = SimpleCache(timeout=10)
+    class Meta:
+        queryset = SMSNotification.objects.all()
+        resource_name = "smsnotf"
+        excludes = ['id']
+        authorization = Authorization()
+        always_return_data = True
+        cache = SimpleCache(timeout=10)
 
-	def dehydrate(self, bundle):
-		bundle.data['reminder'] = bundle.obj.reminder.message
-		return bundle
+    def dehydrate(self, bundle):
+        bundle.data['reminder'] = bundle.obj.reminder.message
+        return bundle
